50e4132c92cda0a2e423f6bdb8629e52fc02abf2
Abhishek6416 <abhishekkr6416@gmail.com>
2023-08-23 11:32:24 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Navbar from "./components/navbar"
+import Inventory from "./components/inventory"
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      localStorage.removeItem('token');
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Inventory />
     </div>
   );
 }
